0041f63fc75d7d403894ef4360090cfa367de3a6
Marcus Lau <lau.marcus76@gmail.com>
2020-04-27 20:17:16 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog 
+
+  # a setter method needs: function name=(parameter) to set 
+  def name=(s)
+    @dog_name = s
+  end
+  
+  def name
+    @dog_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+  
+end
